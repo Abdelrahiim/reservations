c26f1ad864d7de2d5ba5442ad1459f6b64e9872a
Abdelrahiim <abdelrhiimabdelmoneim@outlook.com>
2025-01-08 17:41:31 +0200

    set up the repo with db

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: joi.object({
+        MONGODB_URI: joi.string().uri().required(),
+      }),
+    }),
+  ],
+})
+export class ConfigModule {}
